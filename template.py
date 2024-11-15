74d4a6f8016bd5213a630d44555795fc171941f3
DIkshantPatel2210 <sikshantpatel2210@.com>
2024-11-15 23:14:39 +0100

    Create DIRECTORIES & FILES by template.py

---
+++ template.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from pathlib import Path
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = '[%(asctime)s]: %(message)s:'
+)
+
+project_name = 'text-summarizer'
+
+list_of_files = [
+    '.github/workflows/.gitkeep/',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/utils/common.py',
+    f'src/{project_name}/logging/__init__.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
+    'config/config.yaml',
+    'params.yaml',
+    'app.py',
+    'main.py',
+    'Dockerfile',
+    'requirements.txt',
+    'setup.py',
+    'research/trials.ipynb',
+    'test.py'
+  ]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    filedir, filename = os.path.split(file_path)
+    if filedir != '':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f'Creating Directory: {filedir} for the file {filename}')
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,'w') as file:
+            pass
+            logging.info(f'Creating empty file: {file_path}')
+
+    else:
+        logging.info(f'File already exists: {file_path}')
+
+
+
+
+
+
+
+
